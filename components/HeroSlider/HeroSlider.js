7c8ae9b6d487af106bfc11c8e97e49896c77fe18
augoliveira <aug_oliv@hotmail.com>
2021-05-19 01:22:04 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ components/HeroSlider/HeroSlider.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+export default function HeroSlider() {
+  return (
+    <>
+    
+
+    <div id="about-section" class="g-overflow-hidden">
+        <div class="container g-pt-100">
+            <div class="g-pos-rel">
+              <div class="row justify-content-between">
+                <div class="col-lg-5 g-mb-30">
+                  <div class="g-mt-20 mb-5">
+                    <h3 class="h6 text-uppercase g-color-main-light-v1 g-font-weight-600 g-letter-spacing-2 mb-3">Tentáculo</h3>
+                    <h2 class="mb-4">Gerencie seus documentos de um modo eficiente</h2>
+                    <p class="g-line-height-2">Assuma o controle das informações da sua empresa com a Tentáculo</p>
+                  </div>
+
+                  
+                  <div class="media mb-4">
+                    <div class="d-flex mr-4">
+                      <span class="u-icon-v3 g-width-50 g-height-50 g-color-bluegray g-bg-bluegray-opacity-0_1 g-font-size-16 rounded-circle">
+                        <i class="icon-communication-114 u-line-icon-pro"></i>
+                      </span>
+                    </div>
+                    <div class="media-body">
+                      <span class="g-font-weight-700">Serviços</span>
+                      <p class="g-text">Tencnologia</p>
+                    </div>
+                  </div>
+                  
+                  <div class="media mb-4">
+                    <div class="d-flex mr-4">
+                      <span class="u-icon-v3 g-width-50 g-height-50 g-color-primary g-bg-primary-opacity-0_1 g-font-size-16 rounded-circle">
+                        <i class="icon-finance-091 u-line-icon-pro"></i>
+                      </span>
+                    </div>
+                    <div class="media-body">
+                      <span class="g-font-weight-700">Documento</span>
+                      <p class="g-text">No lugar certo</p>
+                    </div>
+                  </div>
+                  
+                  <div class="media mb-4">
+                    <div class="d-flex mr-4">
+                      <span class="u-icon-v3 g-width-50 g-height-50 g-color-red g-bg-red-opacity-0_1 g-font-size-16 rounded-circle">
+                        <i class="icon-communication-116 u-line-icon-pro"></i>
+                      </span>
+                    </div>
+                    <div class="media-body">
+                      <span class="g-font-weight-700">Armazenamento</span>
+                      <p class="g-text">Cloud</p>
+                    </div>
+                  </div>
+                  
+                </div>
+              </div>
+
+              <div class="col-lg-6 g-pos-abs--lg g-top-0--lg g-right-0--lg g-mb-30">
+                <img class="img-fluid w-100" src="../images/Tentáculo-site-Home.png" alt="Image Description"/>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        </>
+      
+          
+            
+          
+        
+          
+        
+  );
+}
---
+++ components/Sobre/Sobre.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+export default function Sobre() {
+  return (
+    <section id="about" class="about section-bg">
+      <div class="container aos-init aos-animate" data-aos="fade-up">
+      
+
+        
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 aos-init aos-animate" data-aos="fade-right" data-aos-delay="100">
+            <img src="assets/img/about.jpg" class="img-fluid" alt=""/>
+          </div>
+
+          <div class="col-lg-4 pt-4 pt-lg-0 content d-flex flex-column justify-content-center aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+            <div class="section-title">
+          <h2>QUEM SOMOS</h2>
+          <h3>Descubra mais <span>Sobre a Tentáculo</span></h3>
+          <p>Tentáculo é um software para empresas que utilizam checklists e planos de ação em auditorias ou inspeções de processos, lojas, fábricas, veículos, pessoas, equipamentos e ambientes.</p>
+        </div>
+            <h3>Nosso objetivo</h3>
+            <p class="fst-italic">
+            promover a transformação digital, combinamos produto e método próprio para proporcionar qualidade, segurança e eficiência aos nossos clientes.
+            </p>
+            <ul>
+              <li>
+                <i class="bx bx-store-alt"></i>
+                <div>
+                  <h5>Ullamco laboris nisi ut aliquip consequat</h5>
+                  <p>Magni facilis facilis repellendus cum excepturi quaerat praesentium libre trade</p>
+                </div>
+              </li>
+              <li>
+                <i class="bx bx-images"></i>
+                <div>
+                  <h5>Magnam soluta odio exercitationem reprehenderi</h5>
+                  <p>Quo totam dolorum at pariatur aut distinctio dolorum laudantium illo direna pasata redi</p>
+                </div>
+              </li>
+            </ul>
+            <p>
+              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+              culpa qui officia deserunt mollit anim id est laborum
+            </p>
+          </div>
+        </div>
+
+      
+    </section>
+  );
+}
